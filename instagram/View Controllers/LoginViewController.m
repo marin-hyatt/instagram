49d4a23192317526ec495a9c9cbbc13640a4fe5d
marin-hyatt <marin.hyatt@gmail.com>
2021-07-06 16:32:08 -0400

    User can log in, signup and login segue to another screen

--- instagram/View Controllers/LoginViewController.m
+++ instagram/View Controllers/LoginViewController.m
@@ -35,12 +35,14 @@
 */
 
 - (IBAction)logInPressed:(id)sender {
+    [self loginUser];
 }
 
 - (IBAction)signUpPressed:(id)sender {
     [self registerUser];
 }
 
+// Signs up a new user
 - (void)registerUser {
     // initialize a user object
     PFUser *newUser = [PFUser user];
@@ -57,6 +59,24 @@
             NSLog(@"User registered successfully");
             
             // manually segue to logged in view
+            [self performSegueWithIdentifier:@"HomeFeedSegue" sender:nil];
+        }
+    }];
+}
+
+// Logs in a registered user
+- (void)loginUser {
+    NSString *username = self.loginView.usernameField.text;
+    NSString *password = self.loginView.passwordField.text;
+    
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User logged in successfully");
+            
+            // display view controller that needs to shown after successful login
+            [self performSegueWithIdentifier:@"HomeFeedSegue" sender:nil];
         }
     }];
 }
