ba0d8cdaeb523e3d90357ea74e9ac056222b0ab1
marin-hyatt <marin.hyatt@gmail.com>
2021-07-07 19:58:12 -0400

    Fixed extra view controller issue with composing a post, autolayout in compose view

--- instagram/View Controllers/ComposeViewController.m
+++ instagram/View Controllers/ComposeViewController.m
@@ -7,11 +7,12 @@
 
 #import "ComposeViewController.h"
 #import "ComposeView.h"
-#import "CameraViewController.h"
 #import "Post.h"
 
-@interface ComposeViewController () <CameraViewControllerDelegate>
+@interface ComposeViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property (strong, nonatomic) IBOutlet ComposeView *composeView;
+- (IBAction)onImagePickerClicked:(UITapGestureRecognizer *)sender;
+
 
 @end
 
@@ -21,9 +22,39 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    // Sets itself as the delegate for camera view controller
+    [self showImagePicker];
     
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    NSLog(@"Finished picking/taking a photo!");
     
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    
+    UIImage *resizedOriginalImage = [self resizeImage:originalImage withSize: CGSizeMake(originalImage.size.width * 0.6, originalImage.size.height * 0.6)];
+    
+    // Pass image back to compose view controller
+    [self.composeView.image setImage:resizedOriginalImage];
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+// Resizes image to get under 10MB limit for Parse
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
 }
 
 - (IBAction)cancelCompose:(UIBarButtonItem *)sender {
@@ -46,18 +77,35 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-- (void)passBackImage:(UIImage *)image {
-    self.composeView.image.image = image;
-}
-
 
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    CameraViewController *cameraController = [segue destinationViewController];
-    cameraController.delegate = self;
+//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+//    CameraViewController *cameraController = [segue destinationViewController];
+//    cameraController.delegate = self;
+//}
+
+
+- (IBAction)onImagePickerClicked:(UITapGestureRecognizer *)sender {
+    [self showImagePicker];
 }
 
+-(void) showImagePicker {
+    // Sets up image picker
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
 
+    // Checks to see if phone or simulator is able to take pictures
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
 @end
